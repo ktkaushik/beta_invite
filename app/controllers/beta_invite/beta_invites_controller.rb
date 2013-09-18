60478bbd83348e12e54acb98313122bf2aa9ff3d
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-09-19 00:18:22 +0530

    adding create action

--- app/controllers/beta_invite/beta_invites_controller.rb
+++ app/controllers/beta_invite/beta_invites_controller.rb
@@ -4,16 +4,21 @@ module BetaInvite
   class BetaInvitesController < ApplicationController
 
     def new
-      # 
-      # TODO
-      # is it necessary to initialize the BetaInvite model ?
-      # Create a form_tag ?
-      # 
-
       @beta_invite = BetaInvite.new
     end
 
     def create
+      email = params[:beta_invite][:email]
+      beta_invite = BetaInvite.new
+      beta_invite.email = email
+      beta_invite.token = SecureRandom.hex(10) #generate a random hex token for all the users
+      if beta_invite.save
+        flash[:success] = "#{email} has been registered for beta invite"
+        redirect_to beta_invites_path
+      else
+        flash[:alert] = beta_invite.errors.full_messages
+        render :new
+      end
     end
   end
 end
