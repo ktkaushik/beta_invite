3d482f0a41de708ad0123620cf4219436d7d4464
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-09-20 01:23:42 +0530

    copies the index and the new action views seamlessly into your rails via the custom generator

--- lib/generators/beta_invite/view_generator.rb
+++ lib/generators/beta_invite/view_generator.rb
@@ -3,10 +3,13 @@ require "rails/generators"
 module BetaInvite
   module Generators
     class ViewGenerator < Rails::Generators::Base
+      source_root File.expand_path('../../../../app/views/beta_invite/beta_invites', __FILE__)
 
       desc "this generator would copy all the view files for beta_invite engine into your rails app"
+
       def create_views
-        directory "#{Rails.root.join('app','views')}", File.expand_path('../../../../app/views/beta_invite', __FILE__)
+        copy_file 'new.html.erb', Rails.root.join('app','views','beta_invite','beta_invites','new.html.erb')
+        copy_file 'index.html.erb', Rails.root.join('app','views','beta_invite','beta_invites','index.html.erb')
       end
     end
   end
