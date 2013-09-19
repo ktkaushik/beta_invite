4f9955712363ff1c94af4236074141009ac64b37
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-09-20 01:25:27 +0530

    renaming the view generator to views generator

--- lib/generators/beta_invite/views_generator.rb
+++ lib/generators/beta_invite/views_generator.rb
@@ -2,7 +2,7 @@ require "rails/generators"
 
 module BetaInvite
   module Generators
-    class ViewGenerator < Rails::Generators::Base
+    class ViewsGenerator < Rails::Generators::Base
       source_root File.expand_path('../../../../app/views/beta_invite/beta_invites', __FILE__)
 
       desc "this generator would copy all the view files for beta_invite engine into your rails app"
