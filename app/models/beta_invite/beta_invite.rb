a58c87fc44a2fa02df46f2fd3e49f4de59ca7459
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-09-21 08:33:53 +0530

    adding model validations for email field and handling the case

--- app/models/beta_invite/beta_invite.rb
+++ app/models/beta_invite/beta_invite.rb
@@ -1,5 +1,8 @@
 module BetaInvite
   class BetaInvite < ActiveRecord::Base
     attr_accessible :email, :token
+    
+    validates_presence_of :email    
+    validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   end
 end
