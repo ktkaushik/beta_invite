4ec5b0d88feadf8fdb285018b13bbebbde3465f4
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-10-06 03:28:20 +0530

    adding the install generator which would copy the beta invite initializer. Skeleton ready.

---
+++ lib/generators/beta_invite/install_generator.rb
@@ -0,0 +1,20 @@
+#
+# TODO: add copy migration file as well. No need for developer to run another command
+#
+module BetaInvite
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      desc "Creates a BetaInvite initializer"
+
+      def copy_initializer
+        template "beta_invite.rb", "config/initializers/beta_invite.rb"
+      end
+
+      def show_readme
+        readme "README" if behavior == :invoke
+      end
+    end
+  end
+end
---
+++ lib/generators/beta_invite/templates/beta_invite.rb
@@ -0,0 +1,1 @@
+# BetaInvite Module coming soon !!
