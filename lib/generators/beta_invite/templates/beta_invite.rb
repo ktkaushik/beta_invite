51e082f63ca0314807129b5d7e73741b072b98dc
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-10-09 13:41:22 +0530

    bug fix for BetaInviteSetup initializers in the templates

--- lib/generators/beta_invite/templates/beta_invite.rb
+++ lib/generators/beta_invite/templates/beta_invite.rb
@@ -1,4 +1,4 @@
-BetaInvite.setup do |config|
+BetaInviteSetup.setup do |config|
   # Setting this to true would send an email to the admins saying that a
   # particular user with a particular email address has requested for an invite
   config.send_email_to_admins = false
@@ -7,4 +7,8 @@ BetaInvite.setup do |config|
   # of the admins who should receive a notification about the user having requested
   # for an invite in an array.
   config.admin_emails = []
+
+  # Configure your mail configuration in the environments file and enter the from email
+  # to send emails
+  config.from_email = 'from@email.com'
 end
