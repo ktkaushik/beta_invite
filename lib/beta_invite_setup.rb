ef37d153cf8d56d2daf575e891bf89e85146e0d8
Kaushik Thirthappa <thirthappa.kaushik@gmail.com>
2013-10-10 18:14:24 +0530

    setting up the module accessor variable for send thank you email

--- lib/beta_invite_setup.rb
+++ lib/beta_invite_setup.rb
@@ -5,6 +5,7 @@ module BetaInviteSetup
   mattr_accessor :from_email
   mattr_accessor :admin_emails
   mattr_accessor :send_email_to_admins
+  mattr_accessor :send_thank_you_email
 
   def self.setup
     yield self
